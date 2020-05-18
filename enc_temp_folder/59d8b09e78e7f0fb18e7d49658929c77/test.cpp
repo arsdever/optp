53257670e70f1935f6a9430ab45dfda4234606ff
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-05-19 00:25:31 +0400

    Configuration moved to entrypoint

--- enc_temp_folder/59d8b09e78e7f0fb18e7d49658929c77/test.cpp
+++ enc_temp_folder/59d8b09e78e7f0fb18e7d49658929c77/test.cpp
@@ -11,19 +11,12 @@
 #include <operation.h>
 #include "fake_node.h"
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-
 struct simpleOpeartion : public optp::operation
 {
 };
 
 int main(int argc, char** argv)
 {
-	spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-	auto file_logger = spdlog::basic_logger_mt("basic_logger", "logs/basic.txt");
-	spdlog::set_default_logger(file_logger);
-
 	optp::optp protocol1("test_config.json");
 
 	while (1) {
--- enc_temp_folder/7d66fe9cbdb2d3b64db8e8ff41bd4b7e/optp.cpp
+++ enc_temp_folder/7d66fe9cbdb2d3b64db8e8ff41bd4b7e/optp.cpp
@@ -32,6 +32,9 @@ namespace optp
 	{
 		startServer();
 		connectToServer();
+		spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+		auto file_logger = spdlog::basic_logger_mt("basic_logger", "logs/basic.txt");
+		spdlog::set_default_logger(file_logger);
 	}
 
 	optp::optp(std::string const& config_file_path)
