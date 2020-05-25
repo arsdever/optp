5e57f773e9e6570c939cbfa359aa384bac6c8996
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-05-26 01:50:45 +0400

    Implemented getting local node addresses

---
+++ network_interfaces.cpp
@@ -0,0 +1,107 @@
+#include "network_interfaces.h"
+#include <sockpp/inet_address.h>
+#include <sockpp/inet6_address.h>
+#include <string>
+#include <vector>
+
+#if defined (_WIN32)
+#include <iphlpapi.h>
+#elif defined (__linux__)
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#endif
+
+static void collectLocalIpAddresses(std::vector<std::shared_ptr<sockpp::sock_address>>& address_list)
+{
+#if defined (_WIN32)
+#pragma comment(lib, "IPHLPAPI.lib")
+	// https://stackoverflow.com/questions/122208/get-the-ip-address-of-local-computer
+	PIP_ADAPTER_ADDRESSES adapter_addresses(NULL);
+	DWORD adapter_addresses_buffer_size = 16000;
+
+	for (int attempts = 0; attempts != 3; ++attempts)
+	{
+		adapter_addresses = (IP_ADAPTER_ADDRESSES*)malloc(adapter_addresses_buffer_size);
+
+		DWORD error = ::GetAdaptersAddresses(
+			AF_UNSPEC,
+			GAA_FLAG_SKIP_ANYCAST |
+			GAA_FLAG_SKIP_MULTICAST |
+			GAA_FLAG_SKIP_DNS_SERVER |
+			GAA_FLAG_SKIP_FRIENDLY_NAME,
+			NULL,
+			adapter_addresses,
+			&adapter_addresses_buffer_size);
+
+		if (ERROR_SUCCESS == error)
+			break;
+		else if (ERROR_BUFFER_OVERFLOW == error)
+		{
+			free(adapter_addresses);
+			adapter_addresses = NULL;
+			continue;
+		}
+		else
+		{
+			free(adapter_addresses);
+			adapter_addresses = NULL;
+			return;
+		}
+	}
+
+	for (PIP_ADAPTER_ADDRESSES adapter = adapter_addresses; adapter != NULL; adapter = adapter->Next)
+	{
+		if (IF_TYPE_SOFTWARE_LOOPBACK == adapter->IfType)
+			continue;
+		
+		for (PIP_ADAPTER_UNICAST_ADDRESS address = adapter->FirstUnicastAddress; NULL != address; address = address->Next)
+		{
+			auto family = address->Address.lpSockaddr->sa_family;
+			if (AF_INET == family)
+			{
+				SOCKADDR_IN* ipv4 = reinterpret_cast<SOCKADDR_IN*>(address->Address.lpSockaddr);
+				address_list.push_back(std::make_shared<sockpp::inet_address>(*ipv4));
+			}
+			else if (AF_INET6 == family)
+			{
+				SOCKADDR_IN6* ipv6 = reinterpret_cast<SOCKADDR_IN6*>(address->Address.lpSockaddr);
+				auto addr = std::make_shared<sockpp::inet6_address>(*ipv6);
+				if (0 == addr->to_string().find("fe"))
+				{
+					char c = addr->to_string()[2];
+					if (c == '8' || c == '9' || c == 'a' || c == 'b')
+						address_list.push_back(addr);
+				}
+			}
+		}
+	}
+
+	free(adapter_addresses);
+	adapter_addresses = NULL;
+#elif defined (__linux__)
+	ifaddrs* interfaces = nullptr;
+	getifaddrs(&interfaces);
+	for (ifaddrs* ifa = interfaces; ifa != NULL; ifa = ifa->ifa_next)
+	{
+		if (!ifa->ifa_addr)
+			continue;
+
+		if (ifa->ifa_addr->sa_family == AF_INET)
+			address_list.push_back(std::make_shared<sockpp::inet_address>(*ifa->ifa_addr));
+
+		else if (ifa->ifa_addr->sa_family == AF_INET6)
+			address_list.push_back(std::make_shared<sockpp::inet6_address>(*ifa->ifa_addr));
+	}
+#endif
+}
+
+namespace optp
+{
+	network_interfaces::network_interfaces()
+	{
+		static std::vector<std::shared_ptr<sockpp::sock_address>> vec;
+		collectLocalIpAddresses(vec);
+	}
+}
---
+++ network_interfaces.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *		Filename:	network_interfaces.h
+ *		Author:		Arsen Gharagyozyan (arsdever), arsen.gharagyozyn.96@gmail.com
+ *
+ * =====================================================================================
+ */
+
+#pragma once
+
+#include <optp_export.h>
+
+namespace optp
+{
+	class OPTP_EXPORT network_interfaces
+	{
+	public:
+		network_interfaces();
+	};
+}
