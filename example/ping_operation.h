684bff5d2a455277e7877b3ca8637c70a2a12d79
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-06-11 22:03:03 +0400

    Introducing ping operation (test version)

---
+++ example/ping_operation.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *		Filename:	example/ping_operation.h
+ *		Author:		Arsen Gharagyozyan (arsdever), arsen.gharagyozyn.96@gmail.com
+ *
+ * =====================================================================================
+ */
+#pragma once
+#include <operation.h>
+
+namespace optp
+{
+    namespace test
+    {
+        class ping_operation : public operation
+        {
+        public:
+            inline ping_operation() {}
+        };
+    }
+}
